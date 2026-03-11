f4df4e5859163da2026aec24692de6ef84bac231
Apolo <161484547+ApoloMor@users.noreply.github.com>
2026-03-11 09:22:27 -0300

    Add files via upload

---
+++ primeiro.py
@@ -0,0 +1,20 @@
+'''
+num1 = int(input("digite o primeiro número: "))
+num2 = int(input("digite o segundo número: "))
+soma = num1 + num2
+print ("A soma dos números é", soma)
+'''
+'''
+ano = int(input("digite seu ano de nascimento: "))
+calculo =  2020 - ano
+print (f"sua idade é: {calculo}")
+'''
+'''
+materia = str(input("digite sua materia:"))
+nota1 = float(input("digite sua 1 nota:"))
+nota2 = float(input("digite sua 2 nota:"))
+nota3 = float(input("digite sua 3 nota:"))
+nota4 = float(input("digite sua 4 nota:"))
+nota_final = float((nota1 + nota2 + nota3 + nota4) / 4)
+print("sua nota em", materia, "é", nota_final)
+'''
